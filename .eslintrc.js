9de0cb86608cca552b133eb882b9477aebd5d8f5
isaac <isaac@cresmail.com>
2018-04-16 15:01:32 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    plugins: ['prettier'],
+    parserOptions: {
+        sourceType: 'module',
+    },
+};
+
+/*
+details about airbnb config rules
+https://github.com/airbnb/javascript
+*/
