a884cac28943b0413f601c5852ec584c66c0d995
isaac <itozer@gmail.com>
2019-07-15 15:58:08 -0700

    converting to list manager example

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-import { ready, createH1 } from './modules/DOM';
-import { presentValue as bequestPresentValue, taxBenefits as bequestTaxBenefits } from './modules/Bequest';
-import { presentValue as giftAnnuityPresentValue, taxBenefits as giftAnnuityTaxBenefits } from './modules/GiftAnnuity';
-import { presentValue as unitrustPresentValue, taxBenefits as unitrustTaxBenefits } from './modules/Unitrust';
+import { ready } from './modules/DOM';
+import * as ListManager from './modules/ListManager';
 
 ready(() => {
-  const wrapper = document.getElementById('wrapper');
-
-  wrapper.appendChild(createH1(`Bequest present value is ${bequestPresentValue()}`));
-  wrapper.appendChild(createH1(`Bequest tax benefits are ${bequestTaxBenefits()}`));
-
-  wrapper.appendChild(document.createElement('br'));
-  wrapper.appendChild(document.createElement('br'));
-
-  wrapper.appendChild(createH1('Gift Annuity present value is ' + giftAnnuityPresentValue()));
-  wrapper.appendChild(createH1('Gift Annuity tax benefits are ' + giftAnnuityTaxBenefits()));
+  ListManager.getList(12345)
+    .then(resp => {
+      console.log(resp);
+    })
+    .catch(e => {
+      console.log(e);
+    });
 });
