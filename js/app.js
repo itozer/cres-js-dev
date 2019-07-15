800d783ea59fe5d6c808e5d0969dfe8a0819b724
isaac <itozer@gmail.com>
2019-07-15 16:42:37 -0700

    show updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-import { ready } from './modules/DOM';
+import { ready, createH1 } from './modules/DOM';
 import * as ListManager from './modules/ListManager';
 
 ready(() => {
+  document.getElementById('wrapper').appendChild(createH1('Isaac'));
+
   ListManager.getList(12345)
     .then(resp => {
       console.log(resp);
